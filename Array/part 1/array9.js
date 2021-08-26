9b59b07ff4aa2e4515a809d9e3f29533854aea88
ausi1997.com <ausafen1018@gmail.com>
2021-08-26 14:18:21 +0530

    union

---
+++ Array/part 1/array9.js
@@ -0,0 +1,24 @@
+// Given two sorted arrays, find their union and intersection.
+
+// Input : arr1[] = {1, 3, 4, 5, 7}
+   //     arr2[] = {2, 3, 5, 6} 
+// Output : Union : {1, 2, 3, 4, 5, 6, 7} 
+   //      Intersection : {3, 5}
+
+   function union(arr1 , arr2){
+       var newArray = [];
+         for(var i = 0; i<arr1.length ; i++){
+             if(!newArray.includes(arr1[i])){
+               newArray.push(arr1[i]);
+             }
+           for(var j = 0; j<arr2.length && arr2[j]<arr1[i]; j++){
+                    if(!newArray.includes(arr2[j])){
+                        newArray.push(arr2[j]);
+                    }
+           }
+             }
+             
+         return newArray;
+   }
+
+   console.log(union([1, 3, 4, 5, 7],[2, 3, 5, 6]));
